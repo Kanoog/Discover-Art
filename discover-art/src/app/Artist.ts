cd4b58529691788174498e4215e0294b9ba7ced6
apafko <ashley.pafko@optum.com>
2023-12-03 09:13:49 -0600

    Updating Explore page

--- discover-art/src/app/Artist.ts
+++ discover-art/src/app/Artist.ts
@@ -1,5 +1,5 @@
 export interface Artist {
-    id?: number;
+    id: number;
     name: string;
     age: number;
     hometown: string;
--- discover-art/src/app/components/expore-page/expore-page.component.ts
+++ discover-art/src/app/components/expore-page/expore-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Artist } from '../../Artist';
+import { ArtistInfoService } from '../../services/artist-info.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-expore-page',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './expore-page.component.scss'
 })
 export class ExporePageComponent {
+  [x: string]: any;
+
+  artists: Artist[] = [];
+
+  constructor(private artistInfoService: ArtistInfoService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.artistInfoService.getArtists().subscribe((artists) => this.artists = artists);
+  }
+
+  navigateToArtist(artistId: number): void {
+    this.router.navigate(['/artist', artistId]);
+  }
 
 }
